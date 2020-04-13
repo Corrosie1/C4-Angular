76c520805492d687e4b2315d156e62b966282a7c
Michel <michel-disbergen@hotmail.com>
2020-04-13 09:00:43 +0200

    first commit - basis opzet

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="./CSS/master.css">
+    <script src="./Javascript/Angular.js" charset="utf-8"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ php/database/credentials/credentials.php
@@ -0,0 +1,8 @@
+<?php
+$host = '172.16.15.129';
+$user = 'Michel';
+$password = '';
+$database = 'eindopdracht_angular';
+//
+$table = 'personen';
+ ?>
