6e37fa18f5d7b3161b8dc1251307bdd02808c961
Michel2 <97466@student.drenthecollege.nl>
2020-04-14 15:02:12 +0200

    tabel sorteren finished

--- AngularTable.js
+++ AngularTable.js
@@ -1,6 +1,23 @@
-var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
-  $http.get("selectTable.php").then(function (response) {
-    $scope.personen = response.data.records;
-  });
-});
+var app = angular
+        .module("myApp", [])
+        .controller('personenCtrl', function($scope, $http) {
+          $http.get("selectTable.php").then(function (response) {
+            $scope.personen = response.data.records;
+          });
+
+          $scope.sortColumn = "name"
+          $scope.reverseSort = false;
+
+          $scope.sortData = function(column) {
+            $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+            $scope.sortColumn = column;
+          }
+
+          $scope.getSortClass = function(column) {
+
+            if ($scope.sortColumn == column) {
+              return $scope.reverseSort ? 'arrow-up' : 'arrow-down';
+            }
+            return '';
+          }
+        });
--- index.php
+++ index.php
@@ -10,31 +10,49 @@
   </head>
   <body>
     <div class="parent">
-      <div class="table-child" ng-app="myApp" ng-controller="customersCtrl">
-        <table>
-          <tr>
-            <th> ID </th>
-            <th> Voornaam  </th>
-            <th> Achternaam </th>
-            <th> Straat </th>
-            <th> Huisnummer </th>
-            <th> Postcode </th>
-            <th> Woonplaats </th>
-            <th> Telefoon nummer </th>
-            <th> Tijdtoegevoegd </th>
-          </tr>
-          <tr ng-repeat="persoon in personen">
-            <td>{{ persoon.ID }}</td>
-            <td>{{ persoon.Voornaam }}</td>
-            <td>{{ persoon.Achternaam }}</td>
-            <td>{{ persoon.Straat }}</td>
-            <td>{{ persoon.Huisnummer }}</td>
-            <td>{{ persoon.Postcode }}</td>
-            <td>{{ persoon.Woonplaats }}</td>
-            <td>{{ persoon.TelefoonNummer }}</td>
-            <td>{{ persoon.Tijdtoegevoegd }}</td>
-          </tr>
-        </table>
+      <div class="borderUnderChild">
+        <h1 class="Mainheader"> C4 Angular eindopdracht - Michel Disbergen </h1>
+        <div class="uitlegSubChild">
+          <h2> Filteren </h2>
+          <p>door op woonplaats of achternaam van de table headers te klikken wordt het mogelijk gemaakt om <br>
+          ascending of descdending te filteren/sorteren </p>
+          <h2> CRUD systeem </h2>
+          <p> onderin het scherm heb je meerdere links.<br>
+          Door op 1 van deze links te klikken wordt wordt er gebruik gemaakt van routing. <br>
+          Hiermee wordt het juiste scherm opgehaald</p>
+          </div>
+        <div class="tabelSubChild" ng-app="myApp" ng-controller="personenCtrl">
+          <h2> Overzicht gegevens </h2>
+          <p> Aan de onderkant is een tabel te zien, deze tabel is een overzicht <br>
+          van alle gegevens die beschikbaar zijn in de database 'eindopdracht_angular'</p>
+          <table align=center>
+            <tr>
+              <th> ID </th>
+              <th> Voornaam </th>
+              <th ng-click="sortData('Achternaam')"> Achternaam <div ng-class="getSortClass('Achternaam')"></div></th>
+              <th> Straat </th>
+              <th> Huisnummer </th>
+              <th> Postcode </th>
+              <th ng-click="sortData('Woonplaats')"> Woonplaats <div ng-class="getSortClass('Woonplaats')"></div> </th>
+              <th> Telefoon nummer </th>
+              <th> Tijdtoegevoegd </th>
+            </tr>
+            <tr ng-repeat="persoon in personen | orderBy:sortColumn:reverseSort">
+              <td>{{ persoon.ID }}</td>
+              <td>{{ persoon.Voornaam }}</td>
+              <td>{{ persoon.Achternaam }}</td>
+              <td>{{ persoon.Straat }}</td>
+              <td>{{ persoon.Huisnummer }}</td>
+              <td>{{ persoon.Postcode }}</td>
+              <td>{{ persoon.Woonplaats }}</td>
+              <td>{{ persoon.TelefoonNummer }}</td>
+              <td>{{ persoon.Tijdtoegevoegd }}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div class="routing" ng-app="routingApp" ng-controller="routingCtrl">
+
       </div>
     </div>
   </body>
