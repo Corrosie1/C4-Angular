2a29f78832166f88bc8d611c4a286a345a31b122
Michel <michel-disbergen@hotmail.com>
2020-04-15 13:26:27 +0200

    CRUD-Delete - working on

--- php/includes/CRUD/delete.php
+++ php/includes/CRUD/delete.php
@@ -1,5 +1,13 @@
-<p>hey.delete</p>
+<h3>Delete</h3>
+<p>in het onderstaande overzicht wordt het mogelijk gemaakt om records te deleten.<br>
+vul het ID in van het record die je graag zou willen van de database</p>
 
-<?php
-  echo "yoo";
- ?>
+<form method="post">
+  <label for="ID-FORM">ID:</label>
+  <input type="text" name="ID-FORM">
+  <br>
+  <br>
+  <input type="submit" name="submit" value="verwijder record">
+</form>
+<br>
+<br>
